467b7fdf838184144a2ccd9a601542d6af3609b1
qiaolaoshi759 <55394195+qiaolaoshi@users.noreply.github.com>
2020-12-29 17:22:46 +0800

    except  mult and div

--- exp4/exp4.srcs/sources_1/new/alu.v
+++ exp4/exp4.srcs/sources_1/new/alu.v
@@ -1,19 +1,118 @@
 
 `include "defines.vh"
 module alu #(WIDTH = 32)
-            (input [WIDTH-1:0] a,
-             input [WIDTH-1:0] b,
+            (input [WIDTH-1:0] a,//操作数1
+             input [WIDTH-1:0] b,//操作数2
              input [7:0] op,
-             output[WIDTH-1:0] res,
+             input [63:0] hilo_i,//乘除法hilo寄存器：输入
+             output [63:0] hilo_o,//乘除法hilo寄存器：输出
+             output reg [WIDTH-1:0] res,
+             output reg overflow,//算术运算溢出
              output zero);
-    
-assign res = (op == `EXE_AND_OP) ? a & b:
-             (op == `EXE_OR_OP) ? a | b:
-             (op == `EXE_ADD_OP) ? a + b:
-             (op == `EXE_SUB_OP) ? a - b:
-             (op == `EXE_SLT_OP) ? (a<b) ? 1 : 0 :
-             8'b0;
 
-assign zero = ((a-b) == 0);
+           reg [WIDTH-1:0] numa,numb;//考虑溢出情况算术运算，中间变量
+           reg [31:0]nresult;//有溢出的算术运算的中间变量结果
+
+        //乘法
+        //除法
+        
+        always@(*)
+         case(op)
+         //算术运算指令
+          `EXE_ADD_OP: //考虑溢出情况：有符号加法
+              begin
+                  nresult <= a + b;
+                  overflow <= (a[31]&b[31]&~nresult[31])|(~a[31]&~b[31]&nresult[31]);
+                  if (overflow == 1'b1)
+                      begin
+                          res <= 32'b0;
+                      end
+                  else begin
+                      res <= a + b;
+                  end
+              end
+         `EXE_ADDU_OP: res<= a + b;//无符号加法
+         `EXE_SUB_OP: //考虑溢出情况，有符号减法
+              begin
+                   numa <= a;
+                   numb <= ~b+1;
+                   nresult <= numa + numb;
+                   overflow <= (numa[31] & numb[31] & ~nresult[31]) | (~numa[31] & ~numb[31] & nresult[31]);
+                   if (overflow == 1'b1) 
+                       begin
+                           res <= 32'b0;
+                       end
+                   else  begin
+                       res <= numa + numb; 
+                   end
+              end
+         `EXE_SUBU_OP://无符号减法
+              begin
+                  numa <= a;
+                  numb <= ~b+1;
+                  res <= numa + numb;
+              end
+         `EXE_SLT_OP://判断第一个操作数是否小于第二个操作数
+              begin  //如果小于，置1，否则置0
+                  numa <= a;
+                  numb <= ~b+1;
+                  nresult <= numa + numb;
+                  if(nresult[31] == 0)
+                    begin
+                       res <= 32'b0;
+                    end
+                  else res <= 32'b1;
+         
+              end
+         `EXE_SLTU_OP: 
+              begin
+                if(a < b) res <= 32'b1;
+                else   res <= 32'b0 ;
+              end
+
+         `EXE_ADDI_OP://立即数加法
+              begin
+                 nresult <= a + b ;
+                 overflow <= (a[31]&b[31]&~nresult[31])|(~a[31]&~b[31]&nresult[31]);
+                 if(overflow == 1'b1) res <= 32'b0;
+                 else res <= a + b;
+              end
+
+         `EXE_ADDIU_OP: //立即数无符号加法
+             begin
+                 res <= a + b;
+             end
+         
+         `EXE_SLTI_OP:
+            begin
+                numa <= a;
+                numb <= ~b+1;
+                nresult <= numa + numa;
+                if (nresult[31]==0) res <=32'b0;
+                else res <= 32'b1;
+            end
+
+         `EXE_SLTIU_OP:
+            begin
+                if(a<b)
+                res <= 32'b1;
+                else res <= 32'b0;
+            end
+          //乘法
+          //除法
+
+          //访存指令
+            `EXE_LB_OP:res <= a + b;
+            `EXE_LBU_OP:res <= a + b;
+            `EXE_LH_OP:res <= a + b;
+            `EXE_LHU_OP:res <= a + b;
+            `EXE_LW_OP:res <= a + b;
+            `EXE_SW_OP:res <= a + b;      
+            `EXE_SB_OP:res <= a + b;
+            `EXE_SH_OP:res <= a + b;
+         endcase
+        
+
+
     
 endmodule
--- exp4/exp4.srcs/sources_1/new/maindec.v
+++ exp4/exp4.srcs/sources_1/new/maindec.v
@@ -11,75 +11,7 @@ module main_decoder(input [5:0] op,
     
     always @(*) begin
         case (op)
-            // R-type
-            6'b000000: begin
-                jump     <= 1'b0;
-                regwrite <= 1'b1;
-                regdst   <= 1'b1;
-                alusrc   <= 1'b0;
-                branch   <= 1'b0;
-                memwrite <= 1'b0;
-                memtoreg <= 1'b0;
-            end
-            // lw
-            6'b100011: begin
-                jump     <= 1'b0;
-                regwrite <= 1'b1;
-                regdst   <= 1'b0;
-                alusrc   <= 1'b1;
-                branch   <= 1'b0;
-                memwrite <= 1'b0;
-                memtoreg <= 1'b1;
-            end
-            // sw
-            6'b101011: begin
-                jump     <= 1'b0;
-                regwrite <= 1'b0;
-                regdst   <= 1'b0;
-                alusrc   <= 1'b1;
-                branch   <= 1'b0;
-                memwrite <= 1'b1;
-                memtoreg <= 1'b0;
-            end
-            // beq
-            6'b000100: begin
-                jump     <= 1'b0;
-                regwrite <= 1'b0;
-                regdst   <= 1'b0;
-                alusrc   <= 1'b0;
-                branch   <= 1'b1;
-                memwrite <= 1'b0;
-                memtoreg <= 1'b0;
-            end
-            // addi
-            6'b001000: begin
-                jump     <= 1'b0;
-                regwrite <= 1'b1;
-                regdst   <= 1'b0;
-                alusrc   <= 1'b1;
-                branch   <= 1'b0;
-                memwrite <= 1'b0;
-                memtoreg <= 1'b0;
-            end
-            // j
-            6'b000010: begin
-                jump     <= 1'b1;
-                regwrite <= 1'b0;
-                regdst   <= 1'b0;
-                alusrc   <= 1'b0;
-                branch   <= 1'b0;
-                memwrite <= 1'b0;
-                memtoreg <= 1'b0;
-            end
-            default: begin
-                jump     <= 1'b0;
-                regwrite <= 1'b0;
-                regdst   <= 1'b0;
-                alusrc   <= 1'b0;
-                branch   <= 1'b0;
-                memwrite <= 1'b0;
-                memtoreg <= 1'b0;
-            end
+           
         endcase
     end
     
