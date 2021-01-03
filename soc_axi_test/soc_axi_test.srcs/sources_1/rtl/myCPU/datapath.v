da9ac69f3a1f466d714b0b66f040b594794bdb16
Bazinga <850462618@qq.com>
2021-01-03 20:47:56 +0800

    改regfile写使能2

--- soc_axi_test/soc_axi_test.srcs/sources_1/rtl/myCPU/datapath.v
+++ soc_axi_test/soc_axi_test.srcs/sources_1/rtl/myCPU/datapath.v
@@ -150,7 +150,7 @@ wire [31:0] aluout2E;
 
 //////////////////////////////////////
 // soc debug
-assign debug_wb_rf_wen = {4{regwriteW & ~inst_stall}}; // 直接扩展为 4 位
+assign debug_wb_rf_wen = {4{regwriteW & ~inst_stall & ~data_stall}}; // 直接扩展为 4 位
 assign debug_wb_rf_wnum = writeregW;
 assign debug_wb_rf_wdata = resultW;
 /////////////////////////////////////
@@ -231,11 +231,11 @@ assign saD = instrD[10: 6];
 
 
 //寄存器堆
-// TODO 等待指令存储器ready的时候，寄存器只写一个周期，否则跟reference对不上
+// TODO 等待存储器ready的时候，寄存器只写一个周期，否则跟reference对不上
 regfile regfile(
 	.clk(~clk),
 	.rst(rst),
-	.we3(regwriteW & ~inst_stall),
+	.we3(regwriteW & ~inst_stall & ~data_stall),
 	.ra1(instrD[25:21]),
 	.ra2(instrD[20:16]),
 	.wa3(writeregW),
