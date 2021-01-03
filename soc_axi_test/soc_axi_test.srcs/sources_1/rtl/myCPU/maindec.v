36bd7d9988f478f55683f0b576ce0bb91f5bfbe2
Bazinga <850462618@qq.com>
2021-01-03 21:31:39 +0800

    有了握手、i_stall之后就不用lwstall了

--- soc_axi_test/soc_axi_test.srcs/sources_1/rtl/myCPU/maindec.v
+++ soc_axi_test/soc_axi_test.srcs/sources_1/rtl/myCPU/maindec.v
@@ -79,21 +79,17 @@ assign lo_write = mul | div | mtlo;
         ri       <= 1'b0;
         mtc0     <= 1'b0;
         mfc0     <= 1'b0;
-        case (op)
-            ////////////////////////////////////////
-            // R-type
-            // 逻辑运算指令（非立即数部分）
-            // 移位指令
-            // 数据移动指令
-            // 算术运算指令（非立即数部分）
-            // 分支跳转 jr and jalr
-            ////////////////////////////////////////
-            `EXE_NOP: begin
-                if (instr[15:11] == 5'b0) begin
-                    // NOP，不解码
-                    regwrite <= 1'b0;
-                    regdst   <= 1'b0;
-                end else begin
+        if (instr[31:0] != 32'b0) begin
+            case (op)
+                ////////////////////////////////////////
+                // R-type
+                // 逻辑运算指令（非立即数部分）
+                // 移位指令
+                // 数据移动指令
+                // 算术运算指令（非立即数部分）
+                // 分支跳转 jr and jalr
+                ////////////////////////////////////////
+                `EXE_NOP: begin
                     regwrite <= 1'b1;
                     regdst   <= 1'b1;
                     if (funct == `EXE_MFHI) begin
@@ -133,213 +129,213 @@ assign lo_write = mul | div | mtlo;
                         regdst   <= 1'b0;
                     end
                 end
-            end
 
-            ////////////////////////////////////////
-            // 逻辑运算指令（立即数部分）
-            ////////////////////////////////////////
-            `EXE_ANDI: begin
-                regwrite <= 1'b1;
-                alusrc   <= 1'b1;
+                ////////////////////////////////////////
+                // 逻辑运算指令（立即数部分）
+                ////////////////////////////////////////
+                `EXE_ANDI: begin
+                    regwrite <= 1'b1;
+                    alusrc   <= 1'b1;
 
-            end
-            `EXE_XORI: begin
-                regwrite <= 1'b1;
-                alusrc   <= 1'b1;
-            end
-            `EXE_LUI: begin
-                regwrite <= 1'b1;
-                alusrc   <= 1'b1;
-            end
-            `EXE_ORI: begin
-                regwrite <= 1'b1;
-                alusrc   <= 1'b1;
-            end
-
+                end
+                `EXE_XORI: begin
+                    regwrite <= 1'b1;
+                    alusrc   <= 1'b1;
+                end
+                `EXE_LUI: begin
+                    regwrite <= 1'b1;
+                    alusrc   <= 1'b1;
+                end
+                `EXE_ORI: begin
+                    regwrite <= 1'b1;
+                    alusrc   <= 1'b1;
+                end
 
 
-            ////////////////////////////////////////
-            // 
-            // 访存指令
-            //
-            ////////////////////////////////////////
-            // lb
-            `EXE_LB: begin
-                regwrite <= 1'b1;
-                alusrc   <= 1'b1;
-                memtoreg <= 1'b1;
-                memread  <= 1'b1;
-                memwrite <= 1'b0;
-            end
-            // lbu
-            `EXE_LBU: begin
-                regwrite <= 1'b1;
-                alusrc   <= 1'b1;
-                memtoreg <= 1'b1;
-                memread  <= 1'b1;
-                memwrite <= 1'b0;
-            end
-            // lh
-            `EXE_LH: begin
-                regwrite <= 1'b1;
-                alusrc   <= 1'b1;
-                memtoreg <= 1'b1;
-                memread  <= 1'b1;
-                memwrite <= 1'b0;
-            end
-            // lhu
-            `EXE_LHU: begin
-                regwrite <= 1'b1;
-                alusrc   <= 1'b1;
-                memtoreg <= 1'b1;
-                memread  <= 1'b1;
-                memwrite <= 1'b0;
-            end
-            // lw
-            `EXE_LW: begin
-                regwrite <= 1'b1;
-                alusrc   <= 1'b1;
-                memtoreg <= 1'b1;
-                memread  <= 1'b1;
-                memwrite <= 1'b0;
-            end
-            // sb
-           `EXE_SB: begin
-                alusrc   <= 1'b1;
-                memread  <= 1'b0;
-                memwrite <= 1'b1;
-            end
-            // sh
-           `EXE_SH: begin
-                alusrc   <= 1'b1;
-                memread  <= 1'b0;
-                memwrite <= 1'b1;
-            end
-            // sw
-            `EXE_SW: begin
-                alusrc   <= 1'b1;
-                memread  <= 1'b0;
-                memwrite <= 1'b1;
-            end
 
-            ////////////////////////////////////////
-            //
-            // 算术运算指令
-            //
-            ////////////////////////////////////////
+                ////////////////////////////////////////
+                // 
+                // 访存指令
+                //
+                ////////////////////////////////////////
+                // lb
+                `EXE_LB: begin
+                    regwrite <= 1'b1;
+                    alusrc   <= 1'b1;
+                    memtoreg <= 1'b1;
+                    memread  <= 1'b1;
+                    memwrite <= 1'b0;
+                end
+                // lbu
+                `EXE_LBU: begin
+                    regwrite <= 1'b1;
+                    alusrc   <= 1'b1;
+                    memtoreg <= 1'b1;
+                    memread  <= 1'b1;
+                    memwrite <= 1'b0;
+                end
+                // lh
+                `EXE_LH: begin
+                    regwrite <= 1'b1;
+                    alusrc   <= 1'b1;
+                    memtoreg <= 1'b1;
+                    memread  <= 1'b1;
+                    memwrite <= 1'b0;
+                end
+                // lhu
+                `EXE_LHU: begin
+                    regwrite <= 1'b1;
+                    alusrc   <= 1'b1;
+                    memtoreg <= 1'b1;
+                    memread  <= 1'b1;
+                    memwrite <= 1'b0;
+                end
+                // lw
+                `EXE_LW: begin
+                    regwrite <= 1'b1;
+                    alusrc   <= 1'b1;
+                    memtoreg <= 1'b1;
+                    memread  <= 1'b1;
+                    memwrite <= 1'b0;
+                end
+                // sb
+            `EXE_SB: begin
+                    alusrc   <= 1'b1;
+                    memread  <= 1'b0;
+                    memwrite <= 1'b1;
+                end
+                // sh
+            `EXE_SH: begin
+                    alusrc   <= 1'b1;
+                    memread  <= 1'b0;
+                    memwrite <= 1'b1;
+                end
+                // sw
+                `EXE_SW: begin
+                    alusrc   <= 1'b1;
+                    memread  <= 1'b0;
+                    memwrite <= 1'b1;
+                end
+
+                ////////////////////////////////////////
+                //
+                // 算术运算指令
+                //
+                ////////////////////////////////////////
 
 
-            `EXE_ADDI: begin
-                regwrite <= 1'b1;
-                alusrc   <= 1'b1;
-            end
-            `EXE_ADDIU: begin
-                regwrite <= 1'b1;
-                alusrc   <= 1'b1;
-            end
-            `EXE_SLTI: begin
-                regwrite <= 1'b1;
-                alusrc   <= 1'b1;
-            end
-            `EXE_SLTIU: begin
-                regwrite <= 1'b1;
-                alusrc   <= 1'b1;
-            end
+                `EXE_ADDI: begin
+                    regwrite <= 1'b1;
+                    alusrc   <= 1'b1;
+                end
+                `EXE_ADDIU: begin
+                    regwrite <= 1'b1;
+                    alusrc   <= 1'b1;
+                end
+                `EXE_SLTI: begin
+                    regwrite <= 1'b1;
+                    alusrc   <= 1'b1;
+                end
+                `EXE_SLTIU: begin
+                    regwrite <= 1'b1;
+                    alusrc   <= 1'b1;
+                end
 
-            ////////////////////////////////////////
-            //
-            // 分支跳转指令
-            //
-            ////////////////////////////////////////
-            // j
-            `EXE_J: begin
-                jump     <= 1'b1;
-            end
-            // jal : 需要写寄存器
-            `EXE_JAL: begin
-                regwrite <= 1'b1;
-                jal      <= 1'b1;
-                // jump     <= 1'b1;
-            end
-            
-            // 所有分支指令的第0-15bit存储的都是offset
-            // 如果发生转移，那么将offset左移2位，并符号扩展至32位
-            // 然后与延迟槽指令的地址相加，加法的结果就是转移目的地址
-            // beq
-            // if rs = rt then branch
-            `EXE_BEQ: begin
-                branch   <= 1'b1;
-            end
-            // bgtz
-            // if rs > 0 then branch
-            `EXE_BGTZ: begin
-                branch   <= 1'b1;
-            end
-            // blez
-            // if rs <= 0 then branch
-            `EXE_BLEZ: begin
-                branch   <= 1'b1;
-            end
-            // bne
-            // if rs != rt then branch
-            `EXE_BNE: begin
-                branch   <= 1'b1;
-            end
-            `EXE_REGIMM_INST: begin
-                case (rt)
-                    // bltz
-                    // if rs < 0 then branch
-                    `EXE_BLTZ: begin
-                        branch   <= 1'b1;
-                    end
-                    // bgez
-                    // if rs >= 0 then branch
-                    `EXE_BGEZ: begin
-                        branch   <= 1'b1;
-                    end
-                    // bltzal
-                    // if rs < 0 then branch
-                    `EXE_BLTZAL: begin
-                        regwrite <= 1'b1;
-                        branch   <= 1'b1;
-                        bal      <= 1'b1;
-                    end
-                    // bgezal
-                    // if rs >= 0 then branch
-                    `EXE_BGEZAL: begin
-                        regwrite <= 1'b1;
-                        branch   <= 1'b1;
-                        bal      <= 1'b1;
-                    end
-                endcase
-            end
+                ////////////////////////////////////////
+                //
+                // 分支跳转指令
+                //
+                ////////////////////////////////////////
+                // j
+                `EXE_J: begin
+                    jump     <= 1'b1;
+                end
+                // jal : 需要写寄存器
+                `EXE_JAL: begin
+                    regwrite <= 1'b1;
+                    jal      <= 1'b1;
+                    // jump     <= 1'b1;
+                end
+                
+                // 所有分支指令的第0-15bit存储的都是offset
+                // 如果发生转移，那么将offset左移2位，并符号扩展至32位
+                // 然后与延迟槽指令的地址相加，加法的结果就是转移目的地址
+                // beq
+                // if rs = rt then branch
+                `EXE_BEQ: begin
+                    branch   <= 1'b1;
+                end
+                // bgtz
+                // if rs > 0 then branch
+                `EXE_BGTZ: begin
+                    branch   <= 1'b1;
+                end
+                // blez
+                // if rs <= 0 then branch
+                `EXE_BLEZ: begin
+                    branch   <= 1'b1;
+                end
+                // bne
+                // if rs != rt then branch
+                `EXE_BNE: begin
+                    branch   <= 1'b1;
+                end
+                `EXE_REGIMM_INST: begin
+                    case (rt)
+                        // bltz
+                        // if rs < 0 then branch
+                        `EXE_BLTZ: begin
+                            branch   <= 1'b1;
+                        end
+                        // bgez
+                        // if rs >= 0 then branch
+                        `EXE_BGEZ: begin
+                            branch   <= 1'b1;
+                        end
+                        // bltzal
+                        // if rs < 0 then branch
+                        `EXE_BLTZAL: begin
+                            regwrite <= 1'b1;
+                            branch   <= 1'b1;
+                            bal      <= 1'b1;
+                        end
+                        // bgezal
+                        // if rs >= 0 then branch
+                        `EXE_BGEZAL: begin
+                            regwrite <= 1'b1;
+                            branch   <= 1'b1;
+                            bal      <= 1'b1;
+                        end
+                    endcase
+                end
 
-            // 特权指令
-            6'b010000:
-                case (rs)
-                    5'b00100: begin
-                        mtc0     <= 1'b1;
-                        regwrite <= 1'b0;
-                        regdst   <= 1'b0;
-                    end
-                    5'b00000: begin
-                    // mfc0 解析成 (regwrite=1,regdst=0)，可以保证寄存器堆数据被前推，下一条读了rt的指令取出来的是被前推的数据
-                        mfc0     <= 1'b1;
-                        regwrite <= 1'b1;
-                        regdst   <= 1'b0;
-                    end
-                    default: begin
-                        mtc0 <= 1'b0;
-                        mfc0 <= 1'b0;
-                    end
-                endcase
+                // 特权指令
+                6'b010000:
+                    case (rs)
+                        5'b00100: begin
+                            mtc0     <= 1'b1;
+                            regwrite <= 1'b0;
+                            regdst   <= 1'b0;
+                        end
+                        5'b00000: begin
+                        // mfc0 解析成 (regwrite=1,regdst=0)，可以保证寄存器堆数据被前推，下一条读了rt的指令取出来的是被前推的数据
+                            mfc0     <= 1'b1;
+                            regwrite <= 1'b1;
+                            regdst   <= 1'b0;
+                        end
+                        default: begin
+                            mtc0 <= 1'b0;
+                            mfc0 <= 1'b0;
+                        end
+                    endcase
 
-            default: begin
-                // 没有匹配的op则触发保留指令例外
-                //FIXME 只有op不匹配时才触发保留指令，有没有可能op匹配也是保留指令呢
-                ri <= 1'b1;
-            end
-        endcase
+                default: begin
+                    // 没有匹配的op则触发保留指令例外
+                    //FIXME 只有op不匹配时才触发保留指令，有没有可能op匹配也是保留指令呢
+                    ri <= 1'b1;
+                end
+            endcase
+        end
     end
     
 endmodule
