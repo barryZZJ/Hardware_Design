532bab32a2115d583052001c8f59b62595b064eb
Bazinga <850462618@qq.com>
2021-01-04 16:54:00 +0800

    实际上没解决

--- soc_axi_test/soc_axi_test.srcs/sources_1/rtl/myCPU/hazard.v
+++ soc_axi_test/soc_axi_test.srcs/sources_1/rtl/myCPU/hazard.v
@@ -105,7 +105,7 @@ assign stallW = (inst_stall || data_stall) && ~flushExcept;
 
 // 只要有一个在stall，CPU就处于stall状态
 //TODO 可能还要改
-assign longest_stall = inst_stall || data_stall || divstallE;
+assign longest_stall = (inst_stall || data_stall || divstallE) && ~flushExcept;
 
 // 除法要么stallM，要么flushM，如果是stall的话，就是前一条指令在M一直写memory，如果是flush就正常清空了
 // TODO 要不要把每个flush信号都带上~inst_stall
